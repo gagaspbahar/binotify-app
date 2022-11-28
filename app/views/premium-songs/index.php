3ba29410792f47e3995c4ee3d4b527ce1959ecbe
lyorafelicya <13520073@std.stei.itb.ac.id>
2022-11-28 20:06:30 +0700

    fix: add premium song player

--- app/views/premium-songs/index.php
+++ app/views/premium-songs/index.php
@@ -9,7 +9,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title> Binotify </title>
-        <link rel="stylesheet" href="../../../public/css/premiumsongs.css" />
+        <link rel="stylesheet" href="../../../public/css/premiumsong.css" />
+        <!-- <link rel="stylesheet" href="../../../public/css/songdetail.css" /> -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <script src="../../../public/js/utility.js"></script>
@@ -43,8 +44,6 @@
 
                     <div class="song-container">
                         <h1 class="title"> Songs by Tulus </h1>
-
-                        <!-- <h2 class="top-10-newest">Top 10</h2> -->
 
                         <div class="songlist-container">
 
@@ -83,15 +82,21 @@
                                         echo "  
                                             <li class='songlist-row'>
                                                 <div class='song-count'>
-                                                    <a href='/?detailalbum'>
-                                                        <img class='play' src='../../../public/img/play-white.png'>
-                                                    </a>
+                                                    
+                                                    <button class='controlButton play list' title='Play' onclick='playSong();'>
+                                                        <img src='../../../public/img/play-white.png'>
+                                                    </button>
+
+                                                    <button class='controlButton pause list' title='Pause' onclick='pauseSong();'>
+                                                        <img src='../../../public/img/pause-white.png'>
+                                                    </button>
+                                                 
                                                     <span class='song-number'> $count</span>
                                                 </div>
                 
                                                 <div class='song-info'>
                                                     
-                                                    <span class='song-title'><a class='detail' href='/?song/$song[song_id]'>$song[judul]</a></span>
+                                                    <span class='song-title'>$song[judul]</span>
                                                     
                                                 </div>
                 
@@ -111,6 +116,83 @@
                     </div>
                 </div>
             </div>
+
+            <div id="song-player-container">
+                <div id="song-player-bar">
+                    <div id="songplayer-left">
+                        <div class="content">
+                            <!-- <span class="album-image">
+                                <img id="song-album-image" src="../../../public/img/binomify-logo.png" alt="album" class="album-img">
+                            </span> -->
+                            <div class="trackInfo">
+                                <span class="trackName pointer">
+                                    <p class="song-track-title"> No Song Chosen.. </p>
+                                </span>
+                                <!-- <span class="artistName pointer">
+                                    <span class="song-track-artist"> loading artist..</span>
+                                </span> -->
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div id="songplayer-center">
+                        <div class="content playerControls">
+                            <div class="buttons">
+                                <button class="controlButton shuffle" title="Shuffle" onclick="setShuffle();">
+                                    <i class="fas fa-random"></i>
+                                </button>
+
+                                <button class="controlButton previous" title="Previous" onclick="previousSong();">
+                                    <i class="fas fa-step-backward"></i>
+                                </button>
+
+                                <button class="controlButton play" title="Play" onclick="playSong(isGuest);">
+                                    <img src="../../../public/img/play.png" alt="Play">
+                                </button>
+
+                                <button class="controlButton pause" title="Pause" onclick="pauseSong();">
+                                    <img src="../../../public/img/pause.png" alt="Pause">
+                                </button>
+
+                                <button class="controlButton next" title="Next" onclick="nextSong();">
+                                    <i class="fas fa-step-forward"></i>
+                                </button>
+
+                                <button class="controlButton repeat" title="Repeat" onclick="setRepeat();">
+                                    <i class="fas fa-undo-alt"></i>
+                                </button>
+                            </div>
+
+                            <div class="playbackBar">
+                                <span class="progressTime current" id="currentStart">0.00</span>
+                                <div class="progressBar">
+                                    <input type="range" id="seek" class="progressBarBG" value="0" min="0" max="100">
+                                </div>
+                                <span class="progressTime end" id="currentEnd">0.00</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="songplayer-right">
+                        <div class="volumeBar">
+                            <button class="controlButton volume" title="Volume" onclick="setMute();">
+                                <i class="fas fa-volume-down"></i>
+                            </button>
+                            <div class="progressBar">
+                                <div class="progressBarBG">
+                                    <div class="progress"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
+        <script src="../../../public/js/songdetail.js"></script>
+        <script>
+            // getSongDetail(<?php echo $data['id'] ?>);
+        </script>
     </body>
 </html>
